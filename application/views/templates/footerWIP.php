bcae09976fa1afc2866858496f1cf7e2a4bfc1d6
mandyfoster <mandy.s.foster@gmail.com>
2015-08-19 23:36:28 -0700

    Create temp layout

---
+++ application/views/templates/footerWIP.php
@@ -0,0 +1,15 @@
+<?php
+// views/templates/footerWIP.php
+$currentYear = date("Y");
+?>
+
+    <div id="footer">
+        <ul class="footer">
+          <li>&copy; MF ThatsRight <?=$currentYear;?></li>
+          <li>All Rights Reserved</li>
+          <li><a href="#">Design by Mandy Foster</a></li>
+        </ul>
+    </div>
+
+</body>
+</html>
